a03b06aacb3389099d5d9ab842c267513406998d
Christopher Jasinski <89271300+Rytsumusic@users.noreply.github.com>
2024-04-27 16:45:21 -0400

    tried to get treemap working need to get the images and labels on the treemap

--- chest_xray_clustering.py
+++ chest_xray_clustering.py
@@ -8,6 +8,7 @@ from keras import layers, models, preprocessing, applications
 import sklearn
 from sklearn.cluster import HDBSCAN
 from sklearn.manifold import TSNE
+import squarify
 
 model = models.load_model('./models/VGG16.hdf5')
 model.summary()
@@ -28,36 +29,65 @@ with rich.progress.Progress() as progress:
         img_array = np.expand_dims(img_array, axis=0)
         img_array = applications.vgg16.preprocess_input(img_array)
         embedding = embedding_model.predict(img_array)
-        embeddings.append(embedding)  # Append the embedding to the list
+        embeddings.append(embedding)
         progress.update(task, advance=1)
 
 embeddings = np.concatenate(embeddings, axis=0)
         
 print(embeddings)
 
-# HDBSCAN
+# Clustering
 if len(embeddings) > 1:
-    HDBSCAN = sklearn.cluster.HDBSCAN(min_cluster_size=5, metric='euclidean').fit(embeddings)
+    HDBSCAN = sklearn.cluster.HDBSCAN(min_cluster_size=2, metric='euclidean').fit(embeddings)
     clusters = HDBSCAN.fit_predict(embeddings)
+    
 else:
     print("Not enough samples for HDBSCAN clustering.")
 print("clusters",clusters)
 
-# TSNE
+
+
+
+
 tsne = TSNE(n_components=2, perplexity=70).fit_transform(embeddings)
-# plot actual images
+
+# Regular clustering mapping to show the clustering works. Centroids are marked with red X.
 fig, ax = plt.subplots()
 for i, txt in enumerate(image_paths):
     img = preprocessing.image.load_img(txt, target_size=(224, 224))
     img = np.array(img)
     ax.imshow(img, extent=(tsne[i, 0], tsne[i, 0]+0.5, tsne[i, 1], tsne[i, 1]+0.5))
-    #ax.annotate("", (tsne[i, 0], tsne[i, 1]))  # Remove the txt argument to remove the path annotation
+
+centroids = np.zeros((len(np.unique(clusters)), 2))
+for i, cluster_label in enumerate(np.unique(clusters)):
+    cluster_points = tsne[clusters == cluster_label]
+    centroid = np.mean(cluster_points, axis=0)
+    centroids[i] = centroid
+    ax.scatter(centroid[0], centroid[1], marker='o', color='Green',)
+    
 ax.set_xlim(tsne[:, 0].min(), tsne[:, 0].max())
 ax.set_ylim(tsne[:, 1].min(), tsne[:, 1].max())
-# remove axis
 ax.axis("off")
 fig.savefig("tsne.png", dpi=1200)
-#plt.scatter(tsne[:, 0], tsne[:, 1])
-#plt.savefig("tsne.png", dpi=1200)
 plt.show()
 
+
+
+clusters = [c for c in clusters if c >= 0]
+cluster_sizes = np.bincount(clusters)
+cluster_labels = np.unique(clusters)
+
+normalized_sizes = cluster_sizes / cluster_sizes.sum()
+
+fig, ax = plt.subplots(figsize=(10, 10))
+squarify.plot(sizes=normalized_sizes, label=cluster_labels, alpha=0.8, ax=ax)
+
+#for i, centroid in enumerate(centroids):
+ #   img = preprocessing.image.load_img(image_paths[i], target_size=(224, 224))
+  #  img = np.array(img)
+   # ax.imshow(img, extent=(centroid[0]-0.25, centroid[0]+0.25, centroid[1]-0.25, centroid[1]+0.25))
+    #ax.scatter(centroid[0], centroid[1])
+plt.axis('off')
+plt.title('Cluster Treemap')
+plt.savefig('treemap.png', dpi=1200)
+plt.show()
