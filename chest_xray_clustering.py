16370e63e443f207918e7c5dea8defe54e5f2c6d
Christopher Jasinski <89271300+Rytsumusic@users.noreply.github.com>
2024-04-26 23:51:59 -0400

    Model is loading efficiently and embedding images with a rich time bar. Embeddings are printed.
    HELL YEAH CODE BABY

--- chest_xray_clustering.py
+++ chest_xray_clustering.py
@@ -6,29 +6,24 @@ import matplotlib.pyplot as plt
 import rich.progress
 from keras import layers, models, preprocessing, applications
 
-model = applications.VGG16(weights='imagenet', include_top=False)
-image_paths = glob.glob('/chest_xray/test/**/*jpeg',recursive=True)
+model = models.load_model('./models/VGG16.hdf5')
+model.summary()
+print("sucessfully loaded model",model.output_shape)
+embedding_output = model.get_layer('global_average_pooling2d_1').output
+embedding_model = models.Model(inputs=model.input, outputs=embedding_output)
+print("embedding shape",embedding_output.shape)
 
-embeddings = []
-
+image_paths = list(glob.glob('..\\Pnemonia-Xray-Treemap\\chest_xray\\test\\**\\*.jpeg',recursive=True))
 with rich.progress.Progress() as progress:
     task = progress.add_task("[red]Embedding images...", total=len(image_paths))
-    try:
+    for f in image_paths:
+        print("number of images",len(image_paths))
+        img = preprocessing.image.load_img(f, target_size=(224, 224))
+        img_array = preprocessing.image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = applications.vgg16.preprocess_input(img_array)
+        embedding = embedding_model.predict(img_array)
+        progress.update(task, advance=1)
+        print(embedding)
 
-        for image_path in image_paths:
-            try:
-                print(f"Processing image {image_path}")
-                image = preprocessing.image.load_img(image_path, target_size=(224, 224))
-                image = preprocessing.image.img_to_array(image)
-                image = np.expand_dims(image, axis=0)
-                image = applications.vgg16.preprocess_input(image)
-                embedding = model.predict(image)
-                embeddings.append(embedding)
-                progress.update(task, advance=1)
-                progress.refresh()
-                print(embedding.shape)
-            except Exception as e:
-                print(f"Failed to process image {image_path}. Error: {e}")
-    except Exception as e:
-        print(f"Error occurred: {e}")
 
