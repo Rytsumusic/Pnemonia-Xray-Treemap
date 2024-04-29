195e3c4f2bffa4726a2982f781673c1aedd3638d
Rytsumusic <89271300+Rytsumusic@users.noreply.github.com>
2024-04-29 16:58:15 -0400

    changed to Medoid to try to solve image issue

--- chest_xray_clustering.py
+++ chest_xray_clustering.py
@@ -5,6 +5,10 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import rich.progress
 from keras import layers, models, preprocessing, applications
+from sklearn.metrics import pairwise_distances
+from pyclustering.cluster.kmedoids import kmedoids
+
+
 import sklearn
 from sklearn.cluster import HDBSCAN
 from sklearn.manifold import TSNE
@@ -18,7 +22,10 @@ embedding_model = models.Model(inputs=model.input, outputs=embedding_output)
 print("embedding shape",embedding_output.shape)
 
 
-image_paths = list(glob.glob('..\\Pnemonia-Xray-Treemap\\chest_xray\\test\\**\\*.jpeg',recursive=True))
+#image_paths = list(glob.glob('..\\Pnemonia-Xray-Treemap\\chest_xray\\test\\**\\*.jpeg',recursive=True))
+image_paths = list(glob.glob('../Pnemonia-Xray-Treemap/chest_xray/test/**/*.jpeg',recursive=True))
+
+
 embeddings = []  
 with rich.progress.Progress() as progress:
     task = progress.add_task("[red]Embedding images...", total=len(image_paths))
@@ -32,63 +39,65 @@ with rich.progress.Progress() as progress:
         embeddings.append(embedding)
         progress.update(task, advance=1)
 
-embeddings = np.concatenate(embeddings, axis=0)
+
         
 print(embeddings)
 
 # Clustering
 if len(embeddings) > 1:
-    HDBSCAN = sklearn.cluster.HDBSCAN(min_cluster_size=2, metric='euclidean').fit(embeddings)
-    clusters = HDBSCAN.fit_predict(embeddings)
-    
+    # Reshape embeddings to have 2 dimensions
+    embeddings_2d = np.squeeze(embeddings)
+    embeddings_2d = embeddings_2d.reshape(embeddings_2d.shape[0], -1)
+    k = 5  # Number of clusters
+    kmedoids_instance = kmedoids(embeddings_2d, initial_index_medoids=np.random.choice(len(embeddings_2d), k, replace=False))
+    kmedoids_instance.process()
+    clusters = kmedoids_instance.get_clusters()
+    medoids = kmedoids_instance.get_medoids()
 else:
-    print("Not enough samples for HDBSCAN clustering.")
-print("clusters",clusters)
+    print("Not enough samples for clustering.")
+
+print("clusters", clusters)
+print("medoids", medoids)
 
 
+tsne = TSNE(n_components=2, perplexity=70).fit_transform(embeddings_2d)
 
 
-
-tsne = TSNE(n_components=2, perplexity=70).fit_transform(embeddings)
-
-# Regular clustering mapping to show the clustering works. Centroids are marked with red X.
 fig, ax = plt.subplots()
 for i, txt in enumerate(image_paths):
     img = preprocessing.image.load_img(txt, target_size=(224, 224))
     img = np.array(img)
     ax.imshow(img, extent=(tsne[i, 0], tsne[i, 0]+0.5, tsne[i, 1], tsne[i, 1]+0.5))
+    #ax.scatter(tsne[i, 0], tsne[i, 1], marker='o', color='blue')
+    for medoid in medoids:
+        ax.scatter(tsne[medoid, 0], tsne[medoid, 1], marker='o', color='red')
 
-centroids = np.zeros((len(np.unique(clusters)), 2))
-for i, cluster_label in enumerate(np.unique(clusters)):
-    cluster_points = tsne[clusters == cluster_label]
-    centroid = np.mean(cluster_points, axis=0)
-    centroids[i] = centroid
-    ax.scatter(centroid[0], centroid[1], marker='o', color='Green',)
-    
-ax.set_xlim(tsne[:, 0].min(), tsne[:, 0].max())
-ax.set_ylim(tsne[:, 1].min(), tsne[:, 1].max())
+ax.set_xlim(tsne[:, 0].min() - 1, tsne[:, 0].max() + 1)
+ax.set_ylim(tsne[:, 1].min() - 1, tsne[:, 1].max() + 1)
 ax.axis("off")
-fig.savefig("tsne.png", dpi=1200)
+ax.set_title("Clustering with medoids labeled")
+fig.savefig("Clustering with medoids labeled", dpi=1200)
 plt.show()
 
 
 
-clusters = [c for c in clusters if c >= 0]
-cluster_sizes = np.bincount(clusters)
-cluster_labels = np.unique(clusters)
 
-normalized_sizes = cluster_sizes / cluster_sizes.sum()
 
+
+
+# Treemap
 fig, ax = plt.subplots(figsize=(10, 10))
-squarify.plot(sizes=normalized_sizes, label=cluster_labels, alpha=0.8, ax=ax)
-
-#for i, centroid in enumerate(centroids):
- #   img = preprocessing.image.load_img(image_paths[i], target_size=(224, 224))
-  #  img = np.array(img)
-   # ax.imshow(img, extent=(centroid[0]-0.25, centroid[0]+0.25, centroid[1]-0.25, centroid[1]+0.25))
-    #ax.scatter(centroid[0], centroid[1])
+sizes = [1] * len(medoids)  
+colors = ['blue', 'green', 'red', 'yellow', 'orange'][:len(medoids)]  # Add more colors if needed
+labels = [''] * len(medoids)  
+for i, medoid in enumerate(medoids):
+    img = preprocessing.image.load_img(image_paths[medoid], target_size=(224, 224))
+    img = np.array(img)
+    ax.imshow(img, extent=(0, 1, 0, 1))
+    labels[i] = 'Medoid {}'.format(i)
+squarify.plot(sizes=sizes, label=labels, color=colors, alpha=0.8)
 
 plt.axis('off')
-plt.title('Cluster Treemap')
+
 plt.savefig('treemap.png', dpi=1200)
 plt.show()
